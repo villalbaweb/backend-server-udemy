9fc72722f76ebb8aff8beb577b75a1c33877141d
villalbaweb <villalbaweb@gmail.com>
2018-09-08 18:46:25 -0500

    Cambiando a error 400 Bad Request cuando algun parametro no pase las validaciones

--- routes/user.js
+++ routes/user.js
@@ -43,7 +43,7 @@ app.post('/', (req, res) => {
     });
     ususario.save((err, usuarioGuardado) => {
         if(err) {
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 mensaje: 'Error al crear usuario',
                 errors: err
