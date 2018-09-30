0a2775d9d6198e444f2f2abc5482b944525caa7c
villalbaweb <villalbaweb@gmail.com>
2018-09-29 19:22:51 -0500

    Agregando validacions de usuario y password usando funcion compareSync

--- routes/login.js
+++ routes/login.js
@@ -7,10 +7,44 @@ var Usuario = require('../models/usuario');
 
 app.post('/', (req, res) => {
 
-    res.status(200).json({
-        ok: true,
-        message: 'Login response'
+    // retrieving request's body using body-parser
+    var body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+
+        if(err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - email',
+                errors: err
+            });
+        }
+
+        // funcion para compoarar un string entrante contra un string que ya esta encryptado por el hash
+        if (!bcrypt.compareSync (body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - password',
+                errors: err
+            }); 
+        }
+        
+        res.status(200).json({
+            ok: true,
+            body: body,
+            message: 'Login response'
+        });
+
     });
+
 });
 
 module.exports = app;
