cd7e37105dda2a14499a1fb82ad322595aceea24
villalbaweb <villalbaweb@gmail.com>
2018-12-02 10:36:22 -0600

    Agregando express-fileupload npm pack para poder subir archivos al servidor, adicionalmente agregando validaciones

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,47 @@
 var express = require('express');
+var fileUpload = require('express-fileupload'); 
+
 var app = express();
 
-app.get('/', (req, res, next) => {
+// default options
+app.use(fileUpload());
+
+
+app.put('/', (req, res, next) => {
+
+    // check if there is a file attached in the request
+    if(!req.files) {
+        return res.status(400)
+        .json({
+            ok: false,
+            mensaje: 'No selecciono nada',
+            error: { message: 'Debe seleccionar una imagen'}
+        });
+    }
+
+    // obtener nombre del archivo
+    var archivo = req.files.imagen;
+    var nombreCortado = archivo.name.split('.');
+    var extensionArchivo = nombreCortado[nombreCortado.length - 1];
+
+    // just this extensions are valid
+    var extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if(extensionesValidas.indexOf(extensionArchivo) < 0) {
+        return res.status(400)
+        .json({
+            ok: false,
+            mensaje: 'Extension no valida',
+            error: { message: 'Debe seleccionar un archivo con extension png, jpg, gif, jpeg'}
+        });
+    }
+
+
     res.status(200)
     .json({
         ok: true,
-        mensaje: 'Request OK'
+        mensaje: 'Request OK',
+        extension: extensionArchivo
     });
 });
 
