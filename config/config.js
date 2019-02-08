2faa17e28c0b80ffc7d170dce8c73d91b40e7fae
villalbaweb <villalbaweb@gmail.com>
2019-02-08 17:26:01 -0600

    Agregando autenticacion por Google OAuth

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@this@is@a@seed';
+
+// gOOGLE
+module.exports.GOOGLE_CLIENT_ID = '286917515584-kutk0n9p4r9asahbo8ls5lrtql8p1ntf.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '2YxvmXT2H4X6BNw2PMDh7Igu';
--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,45 @@ var app = express();
 
 var Usuario = require('../models/usuario');
 
+
+// gOOGLE
+const { OAuth2Client } = require('google-auth-library');
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+ 
+// ===================================================
+// Autenticación de google
+// ===================================================
+app.post('/google', (req, res) => {
+    var token = req.body.token || '';
+ 
+    const client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_SECRET);
+    const ticket = client.verifyIdToken({
+        idToken: token,
+        audience: GOOGLE_CLIENT_ID
+    });
+ 
+    ticket.then(data => {
+        res.status(200).json({
+            ok: true,
+            ticket: data.payload,
+            userid: data.payload.sub
+        });
+    }).catch(err => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Token no válido',
+                errors: err
+            });
+        }
+    });
+});
+
+
+//==========================================================
+//              Autenticacion Normal
+//==========================================================
 app.post('/', (req, res) => {
 
     // retrieving request's body using body-parser
