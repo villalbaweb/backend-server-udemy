154c3a29b65177b4f958f3393690e4ea2e9c1e16
villalbaweb <villalbaweb@gmail.com>
2019-02-17 20:12:37 -0600

    Agregando middleware para habilitar CORS

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var bodyParser = require('body-parser');
 // Inicializar variables
 var app = express();
 
+// Habilitando CORS 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
